11c0e9978a4378b7170301becdb8860b56a91a3b
johsan123 <johanhs789@gmail.com>
2026-09-09 16:43:30 +0200

    Oppgave 8 og 9

--- Oving_4.py
+++ Oving_4.py
@@ -24,8 +24,8 @@ df["Production"] = df["Production"].str.replace(",",".").astype(float)
 #print(df.index[0])
 
 #Oppgave 6  
-print(df.loc["2026-01-01 03:00"])
-print(df.loc["2026-01-01 00:00:00":"2026-01-02 00:00:00"])
+#print(df.loc["2026-01-01 03:00"])
+#print(df.loc["2026-01-01 00:00:00":"2026-01-02 00:00:00"])
 
 Daglig_last = df.loc["2026-01-01 00:00:00":"2026-01-02 00:00:00"]
 
@@ -36,6 +36,32 @@ plt.ylabel("Effekt")
 plt.legend()
 plt.tight_layout
 plt.xticks(rotation=45)
-plt.savefig("Daglig_last")
-plt.show()
+#plt.savefig("Daglig_last")
+#plt.show()
 
+
+#Del 3
+#Oppgave 7
+
+df["Netto"] = (df["Production"] - df["Consumption"])
+
+#print(df["Netto"])
+
+#Oppgave 8
+max_prod = df["Production"].max()
+print( max_prod)
+
+min_prod = df["Production"].min()
+print(min_prod)
+
+mean_prod = df["Production"].mean()
+print(mean_prod)
+
+#Oppgave 9
+max_netto = df["Netto"].max()
+max_netto_index = df["Production"].idxmax()
+print("max netto og klokkeslett:", max_netto_index, max_netto )
+
+min_netto = df["Netto"].min()
+min_netto_index = df["Production"].idxmax()
+print("min netto og klokkeslett:", min_netto_index, min_netto )
